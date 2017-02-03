1db169fd3f50fa504c7052237e58d22ff54dd6c7
smsharma1 <smsharma@iitk.ac.in>
2017-02-03 22:29:09 +0530

    tokens

--- src/parser.py
+++ src/parser.py
@@ -1,1 +1,17 @@
-start
+import ply.lex as lex
+
+tokens = (
+	'ID',           #Identifirs
+	'NL',	        #Newline
+	'ILT',	        #Integer literals
+	'FLT',          #floating
+	'BLT',		#boolean
+	'CLT',		#Character
+	'SLT',		#String
+	'MSLT',		#multiline string
+	'ES',		#escape sequence
+	'SL',		#symbol literals
+	'WAC',		#white spaces and comments
+	'XML',		#XML
+)
+
