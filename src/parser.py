a081f83b85d900af0e519d1dfa4c2d45ec1bf0c8
smsharma1 <smsharma@iitk.ac.in>
2017-02-05 00:06:09 +0530

    pydot added

--- src/parser.py
+++ src/parser.py
@@ -1,54 +1,34 @@
 # Yacc example
 
 import ply.yacc as yacc
+import pydot
 
 # Get the token map from the lexer.  This is required.
-from calclex import tokens
+from lexer import tokens
+graph = pydot.Dot(graph_type='digraph')
 
 class Node:
     def __init__(self,type,children=None,leaf=None):
          self.type = type
+	 par = pydot.Node(type, style="filled", fillcolor="red")
+	 graph.add_node(par)
          if children:
               self.children = children
+	      for ch in children:
+	          graph.add_edge(pydot.Edge(par, ch))	
          else:
               self.children = [ ]
          self.leaf = leaf
+         for l in leaf:
+	      n = pydot.Node(l, style="filled", fillcolor="red")
+	      graph.add_edge(pydot.Edge(par, n))
 
 def p_literals(p):
-    '''literals : INT | LONG | FLOAT | STRING | CHAR'''
-    p[0] = p[1]
-
-def p_expression_minus(p):
-    'expression : expression MINUS term'
-    p[0] = p[1] - p[3]
-
-def p_expression_term(p):
-    'expression : term'
-    p[0] = p[1]
-
-def p_term_times(p):
-    'term : term TIMES factor'
-    p[0] = p[1] * p[3]
-
-def p_term_div(p):
-    'term : term DIVIDE factor'
-    p[0] = p[1] / p[3]
-
-def p_term_factor(p):
-    'term : factor'
-    p[0] = p[1]
-
-def p_factor_num(p):
-    'factor : NUMBER'
-    p[0] = p[1]
-
-def p_factor_expr(p):
-    'factor : LPAREN expression RPAREN'
-    p[0] = p[2]
-
-# Error rule for syntax errors
-def p_error(p):
-    print("Syntax error in input!")
+    '''literals : INT 
+		| FLOAT 
+		| STRING 
+		| CHAR'''
+    Node("literals",None,[p[1]])
 
 # Build the parser
 parser = yacc.yacc()
@@ -59,5 +39,5 @@ while True:
    except EOFError:
        break
    if not s: continue
-   result = parser.parse(s)
-   print(result)
+   parser.parse(s)
+   graph.write_png('parsetree.png')
