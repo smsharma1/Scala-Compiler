75274f45fcaf83cdd616e638617dd916ab6fc1cf
smsharma1 <smsharma@iitk.ac.in>
2017-02-07 06:01:39 +0530

    changes

--- src/parser3.py
+++ src/parser3.py
@@ -249,10 +249,17 @@ def p_Block(p):
 			| BLOCKOPEN BlockStatements BLOCKCLOSE'''
 
 #<block statements> ::= <block statement> | <block statements> <block statement>
-def Blo
-<block statement> ::= <local variable declaration statement> | <statement>
+def p_BlockStatements(p):
+	'''BlockStatements : BlockStatement 
+					| BlockStatements BlockStatement'''
 
-<local variable declaration statement> ::= <local variable declaration> ;
+#<block statement> ::= <local variable declaration statement> | <statement>
+def p_BlockStatement(p):
+	'''BlockStatement : LocalVariableDeclarationStatement 
+					| Statement'''
+#<local variable declaration statement> ::= <local variable declaration> ;
+def LocalVariableDeclarationStatement(p):
+	'LocalVariableDeclarationStatement : LocalVariableDeclaration'
 
 <local variable declaration> ::= <type> <variable declarators>
 
