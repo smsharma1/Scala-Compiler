36e9551141711120c06012b1f9a1c29fa9fd6cd9
smsharma1 <smsharma@iitk.ac.in>
2017-02-11 02:35:09 +0530

    added two examples

---
+++ tests/test4.scala
@@ -0,0 +1,12 @@
+object Demo {
+   def main(args: Array[String]) {
+      var x = 30;
+      var y = 10;
+      
+      if( x == 30 ){
+         if( y == 10 ){
+            println("X = 30 and Y = 10");
+         }
+      }
+   }
+}
---
+++ tests/test5.scala
@@ -0,0 +1,14 @@
+object Demo {
+   def main(args: Array[String]) {
+      var a = 10;
+      var b = 20;
+      var c = a > b;
+      var d = a >= b;
+      var e = a && b;
+      var f = a || b;
+      var g = a + b;
+      var h = a - b;
+      var i = a ^ b;
+      var g = 1 || 2 && 3 + 4 - 5 * 6 / 7 % 8 ^ 9;
+   }
+}
