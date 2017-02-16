6920c74b329a1306ca04178e18ebe55470633f03
smsharma1 <smsharma@iitk.ac.in>
2017-02-16 21:16:35 +0530

    array example added

--- tests/Good-8.scala
+++ tests/Good-8.scala
@@ -13,14 +13,20 @@ printf(" sum is  %d  && random value is %d",c,d);
 
 
 object Good8 {
+	 def add(arr: Array[Int]) {
+		var c = 0;
+		var i = 0;
+		while (i <= 4) {
+                arr(i) = i+1;
+				c = c + a(i);
+				i = i + 1;
+    	}
+	 }
+
 	def main(args: Array[String]) {
         	var a = new Array[Int](4);
 			var i = 0;
 			var c = 0;
-			while (i <= 4) {
-          //      a(i) = i+1;
-				c = c + a(i);
-				i = i + 1;
-    	}
+			add(a);
 	}
 }
