859962ae3bf3e681bd6b126783fb9179a87960f6
rahulguptakota <grahul@iitk.ac.in>
2017-02-16 20:57:31 +0530

    edited test case

--- tests/Good-11.scala
+++ tests/Good-11.scala
@@ -1,7 +1,7 @@
 object Good11 {
 	def main(args: Array[String]) {
 		println("Enter a value");
-        val x=scala.io.StdIn.readInt();
+        var x=scala.io.StdIn.readInt();
 		if( x > 0 ){
             println(x);
 	    }
