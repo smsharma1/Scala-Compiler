d0de2f6cca3b81d0591378fef3938c4eba06d2d7
smsharma1 <smsharma@iitk.ac.in>
2017-02-16 21:12:11 +0530

    array example added

--- tests/Good-13.scala
+++ tests/Good-13.scala
@@ -3,7 +3,7 @@
 // myint a; /* Here myint should be Data Type*/ // By- Nishit
 object Good12 {
 def main(args: Array[String]) {
-   val long_variable = 1234567;
+   var long_variable: Long = 1234567;
    printf("Size for long long int data type " + SizeEstimator.estimate(long_variable));
    return 0;
 }
--- tests/Good-8.scala
+++ tests/Good-8.scala
@@ -1,10 +1,26 @@
+/*
+// Program to exercise the array feature implemented
+#include<stdio.h>
+void main()
+{
+int a[5]={1,2,3,4,5};
+int c=0,d;
+for(int i=0;i<5;i+)
+        c += a[i];
+printf(" sum is  %d  && random value is %d",c,d);
+}
+*/
+
+
 object Good8 {
 	def main(args: Array[String]) {
-        var a = Array(1, 2, 3, 4, 5);
-        val c=0;
-        for ( i <- 0 to 4) {
-            c += a(i)
-        }
-        println("sum is " + c);
-    }
+        	var a = new Array[Int](4);
+			var i = 0;
+			var c = 0;
+			while (i <= 4) {
+          //      a(i) = i+1;
+				c = c + a(i);
+				i = i + 1;
+    	}
+	}
 }
