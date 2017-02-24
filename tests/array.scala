15be67e6126fa6646fa7f7a1d8ec010a3033ac8b
smsharma1 <smsharma@iitk.ac.in>
2017-02-24 16:51:17 +0530

    "array.scala modified"

--- tests/array.scala
+++ tests/array.scala
@@ -1,18 +1,7 @@
-/*
-// Program to exercise the array feature implemented
-#include<stdio.h>
-void main()
-{
-int a[5]={1,2,3,4,5};
-int c=0,d;
-for(int i=0;i<5;i+)
-        c += a[i];
-printf(" sum is  %d  && random value is %d",c,d);
-}
-*/
-
-object Good8 {
-    def main() {
-	var a = 10;
-    }
+object add {
+   def addInt( a:Int, b:Int ) : Int = {
+      var sum:Int = 0;
+      sum = a + b;
+      return sum;
+   }
 }
