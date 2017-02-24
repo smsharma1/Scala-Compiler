07d380a0caceb6bfdfab60851490d80b4c516c92
smsharma1 <smsharma@iitk.ac.in>
2017-02-24 14:26:32 +0530

    modified node structure

--- tests/Good-8.scala
+++ tests/Good-8.scala
@@ -12,22 +12,16 @@ printf(" sum is  %d  && random value is %d",c,d);
 */
 
 
-/*object Good8 {
+object Good8 {
 	 def add(arr: Array[Int]) {
 		var c = 0;
 		var i = 0;
-		while (i <= 4) {
-                arr[i] = i+1;
-				c = c + a[i];
-				i = i + 1;
-    	}
 	 }
-*/
 	def main(args: Array[String]) {
         	var a = new Array[Int](4);
 			a[i] = 10;
-//			var i = 0;
-//			var c = 0;
-//			add(a);
+			var i = 0;
+			var c = 0;
+			add(a);
 	}
 }
