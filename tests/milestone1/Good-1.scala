74063e72ce4c2095489a475b24a50e16c8c52dfc
smsharma1 <smsharma@iitk.ac.in>
2017-04-22 18:03:35 +0530

    merge

--- tests/milestone1/Good-1.scala
+++ tests/milestone1/Good-1.scala
@@ -1,8 +1,7 @@
 object Good1 {
 	def main(args: Array[String]) {
 	var a : Int = 	55	;
-	println("Double quote (\") is escaped");
-	println(10,12);
-	read(a);
+//	println("Double quote (\") is escaped");
+//	println(10,12);
    }
 }
